d0af35f359368d0a4a3c486d36f649707000d260
slava bily <slavabily@me.com>
2020-09-13 11:28:59 +0300

    Initial Commit

---
+++ Atomic_Element/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Atomic_Element
+//
+//  Created by slava bily on 13.09.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

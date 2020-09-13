caf2baef4f940384b76b760d2c0ccc7ca7576b7c
slava bily <slavabily@me.com>
2020-09-13 16:18:30 +0300

    base element and list of elements strict are added

--- Atomic_Element/main.c
+++ Atomic_Element/main.c
@@ -6,8 +6,48 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, const char * argv[]) {
+typedef struct element { char name[16]; char symb[8]; double weight; } element;
+
+typedef struct list { element data; struct list *next;} list;
+
+int is_empty(const list *l) {
+    return (l == NULL);
+}
+
+list *create_list(element d) {
+    list *head = malloc(sizeof(list));
+    head -> data = d;
+    head -> next = NULL;
+    return head;
+}
+
+list *add_to_front(element d, list *h) {
+    list *head = create_list(d);
+    head -> next = h;
+    return head;
+}
+
+list *array_to_list(element d[], int size) {
+    list *head = create_list(d[0]);
+    int i;
+    for (i = 1; i < size; i++) {
+        head = add_to_front(d[i], head);
+    }
+    return head;
+}
+
+void print_list(list *h, char *title) {
+    printf("%s\n", title);
+    while (!is_empty(h)) {
+        printf("%s :", h -> data.name);
+        h = h -> next;
+    }
+}
+
+int main() {
     // insert code here...
     printf("Hello, World!\n");
     return 0;
